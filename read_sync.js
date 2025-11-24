ead37a899d2570f5167162e6e8725df5f14514a2
Fahim Tazwoar Sourav <tazwoarlives@gmail.com>
2025-11-24 17:15:55 +0600

    read_File async

--- read_sync.js
+++ read_sync.js
@@ -1,7 +1,12 @@
-const fs = require("fs") ;
+const fs = require("fs");
 
 console.log("start reading the file");
 
-const data = fs.readFileSync("./data/diary.txt" , "utf-8") ; 
+try {
+  const data = fs.readFileSync("./data/diary.txt", "utf-8");
+  console.log("file-content", data);
+} catch (error) {
+  console.error(error.message);
+}
 
-console.log(data);
+console.log("finishd");
